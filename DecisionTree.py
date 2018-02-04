32cb6200e0449b0c9d289ff5a486b8ac42b17923
Administrator <Administrator@PC-20170518KDMT>
2018-02-04 18:50:25 +0800

    多数表决法

--- DecisionTree.py
+++ DecisionTree.py
@@ -69,10 +69,9 @@ def chooseBestFeatureToSplit(dataSet):
 def majorityCnt(classList):  
     classCount ={}  
     for vote in classList:  
-        if vote not in classCount.keys():  
-            classCount[vote]=0  
-            classCount[vote]=1  
-            sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)   
+        if vote not in classCount.keys(): classCount[vote]=0  
+        classCount[vote]+=1  
+        sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)   
     return sortedClassCount[0][0] 
 
 #创建决策树
--- MajorityCnt.py
+++ MajorityCnt.py
@@ -7,22 +7,21 @@ import operator
 def majorityCnt(classList):  
     classCount ={}  
     for vote in classList:  
-        if vote not in classCount.keys():  
-            classCount[vote]=0  
-            classCount[vote]=1  
-            sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)   
-    return sortedClassCount[0][0]   
+        if vote not in classCount.keys(): classCount[vote]=0  
+        classCount[vote]+=1  
+        sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)   
+    return sortedClassCount[0][0]
 
 #创建样本数据集  
 def createDataSet():  
-    dataSet = [[1, 1, 'no'], [1, 1, 'no'], [1, 0, 'yes'], [0, 1, 'yes'], [0, 1, 'yes']]  
+    dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]  
     return dataSet  
 
 data = createDataSet()
 
-classList = [example[-1] for example in data] 
+classList = [example[-1] for example in data]
 
-mCnt = majorityCnt(classList)
+mCnt = majorityCnt(classList)  
 
 print('classList:',classList)
 print('majorityCnt:',mCnt)
